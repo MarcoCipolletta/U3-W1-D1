0c8e5fefab32a45e7356c88c6a0fb185a91543f7
MarcoCipolletta <marcocipolletta1998@gmail.com>
2024-10-07 17:04:18 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,45 @@
+interface sim {
+  credito: number;
+  numeroChiamate: number;
+}
+
+class User implements sim {
+  name: string;
+  lastName: string;
+  credito: number = 0;
+  numeroChiamate: number = 0;
+  constructor(_name: string, _lastName: string) {
+    this.name = _name;
+    this.lastName = _lastName;
+  }
+  ricarica(soldi: number): void {
+    this.credito += soldi;
+  }
+  chiamata(minuti: number): void {
+    if (this.credito - minuti * 0.2 < 0) {
+      alert("devi effetuare la ricarica");
+    } else {
+      this.numeroChiamate += minuti;
+      this.credito -= minuti * 0.2;
+    }
+  }
+  chiama404(): number {
+    return this.credito;
+  }
+  getNUmeroChiamata(): number {
+    return this.numeroChiamate;
+  }
+
+  azzeraChiamate(): void {
+    this.numeroChiamate = 0;
+  }
+}
+
+const io = new User("Marco", "Cipolletta");
+
+io.ricarica(50);
+io.chiamata(25);
+console.log(io.chiama404());
+console.log(io.getNUmeroChiamata());
+io.azzeraChiamate();
+console.log(io);
